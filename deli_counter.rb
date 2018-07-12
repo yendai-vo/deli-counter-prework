8c96e6b9f01442477d456a32adb49a52d03c7144
Cindy Vo <yendai.vo@hotmail.com>
2018-07-12 21:17:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,8 +13,8 @@ def take_a_number(katz_deli, name)
     katz_deli.push(name)
     puts "Welcome, #{name}. You are number 1 in line."
   else
-    puts "Welcome, #{name}. You are number #{(index + 1)} in line."
     katz_deli.push(name)
+    puts "Welcome, #{name}. You are number #{(index + 1)} in line."
   end
 end
 
