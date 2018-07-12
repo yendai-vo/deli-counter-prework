7427d22517151144387b379d2e06df86578c1ad2
Cindy Vo <yendai.vo@hotmail.com>
2018-07-12 20:49:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(katz_deli)
-  if (katz_deli.length == 0)
+  if (katz_deli.count == 0)
     "The line is currently empty."
   else
     katz_deli.each_with_index { |person, index|  "The line is currently: #{index+1}. #{person}"}
